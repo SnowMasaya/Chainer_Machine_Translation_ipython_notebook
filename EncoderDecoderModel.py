784a1090b93bd9ac190ce7c6e4211bd943f6a345
SnowMasaya <snow.akogi.pgel@gmail.com>
2015-12-07 08:55:23 +0900

    fix ipython notebook

--- EncoderDecoderModel.py
+++ EncoderDecoderModel.py
@@ -33,46 +33,44 @@ class EncoderDecoderModel:
         self.show_hands_on_number = parameter_dict["show_hands_on_number"] 
         self.show_i_epoch     = parameter_dict["show_i_epoch"] 
 
-    def __make_model(self):
-        self.__model = wrapper.make_model(
+    def make_model(self):
+        self.model = wrapper.make_model(
             # encoder
-            w_xi = functions.EmbedID(len(self.__src_vocab), self.__n_embed),
-            w_ip = functions.Linear(self.__n_embed, 4 * self.__n_hidden),
-            w_pp = functions.Linear(self.__n_hidden, 4 * self.__n_hidden),
+            w_xi = functions.EmbedID(len(self.src_vocab), self.n_embed),
+            w_ip = functions.Linear(self.n_embed, 4 * self.n_hidden),
+            w_pp = functions.Linear(self.n_hidden, 4 * self.n_hidden),
             # decoder
-            w_pq = functions.Linear(self.__n_hidden, 4 * self.__n_hidden),
-            w_qj = functions.Linear(self.__n_hidden, self.__n_embed),
-            w_jy = functions.Linear(self.__n_embed, len(self.__trg_vocab)),
-            w_yq = functions.EmbedID(len(self.__trg_vocab), 4 * self.__n_hidden),
-            w_qq = functions.Linear(self.__n_hidden, 4 * self.__n_hidden),
+            w_pq = functions.Linear(self.n_hidden, 4 * self.n_hidden),
+            w_qj = functions.Linear(self.n_hidden, self.n_embed),
+            w_jy = functions.Linear(self.n_embed, len(self.trg_vocab)),
+            w_yq = functions.EmbedID(len(self.trg_vocab), 4 * self.n_hidden),
+            w_qq = functions.Linear(self.n_hidden, 4 * self.n_hidden),
         )
 
-    @staticmethod
-    def new(src_vocab, trg_vocab, n_embed, n_hidden, parameter_dict):
-        self = EncoderDecoderModel(parameter_dict)
-        self.__src_vocab = src_vocab
-        self.__trg_vocab = trg_vocab
-        self.__n_embed = n_embed
-        self.__n_hidden = n_hidden
-        self.__make_model()
+    def new(self, src_vocab, trg_vocab, n_embed, n_hidden, parameter_dict):
+        self.src_vocab = src_vocab
+        self.trg_vocab = trg_vocab
+        self.n_embed = n_embed
+        self.n_hidden = n_hidden
+        self.make_model()
         return self
 
     def save(self, filename):
         with ModelFile(filename, 'w') as fp:
-            self.__src_vocab.save(fp.get_file_pointer())
-            self.__trg_vocab.save(fp.get_file_pointer())
-            fp.write(self.__n_embed)
-            fp.write(self.__n_hidden)
-            wrapper.begin_model_access(self.__model)
-            fp.write_embed(self.__model.w_xi)
-            fp.write_linear(self.__model.w_ip)
-            fp.write_linear(self.__model.w_pp)
-            fp.write_linear(self.__model.w_pq)
-            fp.write_linear(self.__model.w_qj)
-            fp.write_linear(self.__model.w_jy)
-            fp.write_embed(self.__model.w_yq)
-            fp.write_linear(self.__model.w_qq)
-            wrapper.end_model_access(self.__model)
+            self.src_vocab.save(fp.get_file_pointer())
+            self.trg_vocab.save(fp.get_file_pointer())
+            fp.write(self.n_embed)
+            fp.write(self.n_hidden)
+            wrapper.begin_model_access(self.model)
+            fp.write_embed(self.model.w_xi)
+            fp.write_linear(self.model.w_ip)
+            fp.write_linear(self.model.w_pp)
+            fp.write_linear(self.model.w_pq)
+            fp.write_linear(self.model.w_qj)
+            fp.write_linear(self.model.w_jy)
+            fp.write_embed(self.model.w_yq)
+            fp.write_linear(self.model.w_qq)
+            wrapper.end_model_access(self.model)
 
     @staticmethod
     def load(filename):
@@ -82,91 +80,36 @@ class EncoderDecoderModel:
             self.__trg_vocab = Vocabulary.load(fp.get_file_pointer())
             self.__n_embed = int(fp.read())
             self.__n_hidden = int(fp.read())
-            self.__make_model()
-            wrapper.begin_model_access(self.__model)
-            fp.read_embed(self.__model.w_xi)
-            fp.read_linear(self.__model.w_ip)
-            fp.read_linear(self.__model.w_pp)
-            fp.read_linear(self.__model.w_pq)
-            fp.read_linear(self.__model.w_qj)
-            fp.read_linear(self.__model.w_jy)
-            fp.read_embed(self.__model.w_yq)
-            fp.read_linear(self.__model.w_qq)
-            wrapper.end_model_access(self.__model)
+            self.make_model()
+            wrapper.begin_model_access(self.model)
+            fp.read_embed(self.model.w_xi)
+            fp.read_linear(self.model.w_ip)
+            fp.read_linear(self.model.w_pp)
+            fp.read_linear(self.model.w_pq)
+            fp.read_linear(self.model.w_qj)
+            fp.read_linear(self.model.w_jy)
+            fp.read_embed(self.model.w_yq)
+            fp.read_linear(self.model.w_qq)
+            wrapper.end_model_access(self.model)
         return self
 
     def init_optimizer(self):
         self.__opt = optimizers.AdaGrad(lr=0.01)
-        self.__opt.setup(self.__model)
+        self.__opt.setup(self.model)
 
-    def __forward(self, is_training, src_batch, trg_batch = None, generation_limit = None):
-        m = self.__model
-        tanh = functions.tanh
-        lstm = functions.lstm
-        batch_size = len(src_batch)
-        src_len = len(src_batch[0])
-        src_stoi = self.__src_vocab.stoi
-        trg_stoi = self.__trg_vocab.stoi
-        trg_itos = self.__trg_vocab.itos
-        s_c = wrapper.zeros((batch_size, self.__n_hidden))
-        
-        # encoding
-        s_x = wrapper.make_var([src_stoi('</s>') for _ in range(batch_size)], dtype=np.int32)
-        s_i = tanh(m.w_xi(s_x))
-        s_c, s_p = lstm(s_c, m.w_ip(s_i))
-
-        for l in reversed(range(src_len)):
-            s_x = wrapper.make_var([src_stoi(src_batch[k][l]) for k in range(batch_size)], dtype=np.int32)
-            s_i = tanh(m.w_xi(s_x))
-            s_c, s_p = lstm(s_c, m.w_ip(s_i) + m.w_pp(s_p))
-
-        s_c, s_q = lstm(s_c, m.w_pq(s_p))
-        hyp_batch = [[] for _ in range(batch_size)]
-        
-        # decoding
-        if is_training:
-            accum_loss = wrapper.zeros(())
-            trg_len = len(trg_batch[0])
-            
-            for l in range(trg_len):
-                s_j = tanh(m.w_qj(s_q))
-                r_y = m.w_jy(s_j)
-                s_t = wrapper.make_var([trg_stoi(trg_batch[k][l]) for k in range(batch_size)], dtype=np.int32)
-                accum_loss += functions.softmax_cross_entropy(r_y, s_t)
-                output = wrapper.get_data(r_y).argmax(1)
-
-                for k in range(batch_size):
-                    hyp_batch[k].append(trg_itos(output[k]))
-
-                s_c, s_q = lstm(s_c, m.w_yq(s_t) + m.w_qq(s_q))
-
-            return hyp_batch, accum_loss
-        else:
-            while len(hyp_batch[0]) < generation_limit:
-                s_j = tanh(m.w_qj(s_q))
-                r_y = m.w_jy(s_j)
-                output = wrapper.get_data(r_y).argmax(1)
-
-                for k in range(batch_size):
-                    hyp_batch[k].append(trg_itos(output[k]))
-
-                if all(hyp_batch[k][-1] == '</s>' for k in range(batch_size)): break
-
-                s_y = wrapper.make_var(output, dtype=np.int32)
-                s_c, s_q = lstm(s_c, m.w_yq(s_y) + m.w_qq(s_q))
-            
-            return hyp_batch
+    def forward(self, is_training, src_batch, trg_batch = None, generation_limit = None):
+        pass
 
     def train(self, src_batch, trg_batch):
         self.__opt.zero_grads()
-        hyp_batch, accum_loss = self.__forward(True, src_batch, trg_batch=trg_batch)
+        hyp_batch, accum_loss = self.forward(True, src_batch, trg_batch=trg_batch)
         accum_loss.backward()
         self.__opt.clip_grads(10)
         self.__opt.update()
         return hyp_batch
 
     def predict(self, src_batch, generation_limit):
-        return self.__forward(False, src_batch, generation_limit=generation_limit)
+        return self.forward(False, src_batch, generation_limit=generation_limit)
 
     def train_model(self):
         trace('making vocaburaries ...')
@@ -174,7 +117,7 @@ class EncoderDecoderModel:
         trg_vocab = Vocabulary.new(gens.word_list(self.target), self.vocab)
 
         trace('making model ...')
-        model = EncoderDecoderModel.new(src_vocab, trg_vocab, self.embed, self.hidden, self.parameter_dict)
+        model = self.new(src_vocab, trg_vocab, self.embed, self.hidden, self.parameter_dict)
 
         for i_epoch in range(self.epoch):
             trace('epoch %d/%d: ' % (i_epoch + 1, self.epoch))
@@ -196,7 +139,7 @@ class EncoderDecoderModel:
                 trained += K
 
             trace('saving model ...')
-            model.save(str(model) + '.%03d' % (self.epoch + 1))
+            model.save("ChainerMachineTranslation" + '.%03d' % (self.epoch + 1))
 
         trace('finished.')
 
@@ -225,32 +168,6 @@ class EncoderDecoderModel:
 
         trace('finished.')
 
-    def source_to_words(self, source):
-        line = source.replace("¥n", " ").replace("¥t", " ")
-        for spacer in ["(", ")", "{", "}", "[", "]", ",", ";", ":", "++", "!", "$", '"', "'"]:
-            line = line.replace(spacer, " " + spacer + " ")
-    
-        words = [w.strip() for w in line.split()]
-        return words
-
-    def load_data(self, fname):
-        vocab = {}
-        data_dir = "/Users/smap2/TechCircle/Chainer_Machine_Translation_ipython_notebook/"
-        source_courpas = data_dir + fname
-        source = open('%s' % source_courpas, 'r').read()
-        words = self.source_to_words(source)
-        freq = {}
-        dataset = np.ndarray((len(words),), dtype=np.int32)
-        for i, word in enumerate(words):
-            if word not in vocab:
-                vocab[word] = len(vocab)
-                freq[word] = 0
-            dataset[i] = vocab[word]
-            freq[word] += 1
-
-        print('corpus length:', len(words))
-        print('vocab size:', len(vocab))
-    
     def print_out(self, K, i_epoch, trained, src_batch, trg_batch, hyp_batch):
 
         for k in range(self.show_hands_on_number):
